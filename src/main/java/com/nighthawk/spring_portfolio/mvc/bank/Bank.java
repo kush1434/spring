7be1d87a67c233696fe15fbd5bd0d59148efc83f
hanlunli <hanlunli.007@gmail.com>
2025-03-18 22:14:18 -0700

    bank is now reverted!

--- src/main/java/com/nighthawk/spring_portfolio/mvc/bank/Bank.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/bank/Bank.java
@@ -8,17 +8,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.JoinColumn;
-
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import com.nighthawk.spring_portfolio.mvc.person.Person;
 
 @Data
 @NoArgsConstructor
@@ -35,26 +27,10 @@ public class Bank {
 
     private double balance;
     private double loanAmount;
-
-    @JdbcTypeCode(SqlTypes.JSON)
-    @Column(columnDefinition = "jsonb")
     private List<String> stocksOwned = new ArrayList<>();
-
-    @JdbcTypeCode(SqlTypes.JSON)
-    @Column(columnDefinition = "jsonb")
     private List<Double> gamblingProfit = new ArrayList<>();
-
-    @JdbcTypeCode(SqlTypes.JSON)
-    @Column(columnDefinition = "jsonb")
     private List<Double> adventureGameProfit = new ArrayList<>();
-
-    @JdbcTypeCode(SqlTypes.JSON)
-    @Column(columnDefinition = "jsonb")
     private List<Double> stocksProfit = new ArrayList<>();
-
-    @OneToOne
-    @JoinColumn(name = "person_id", unique = true, nullable = false)
-    private Person person;
 
     public Bank(String username, double balance, double loanAmount, List<String> stocksOwned, List<Double> gamblingProfit, List<Double> adventureGameProfit, List<Double> stocksProfit) {
         this.username = username;
@@ -66,29 +42,35 @@ public class Bank {
         this.stocksProfit = stocksProfit;
     }
 
+    // Method to get gambling profits for the user
     public List<Double> getGamblingProfit(Long uid) {
-        return gamblingProfit;
+        return gamblingProfit;  // Return gambling profit for the user based on UID (now a String)
     }
 
+    // Method to get adventure game profits for the user
     public List<Double> getAdventureGameProfit(Long uid) {
-        return adventureGameProfit;
+        return adventureGameProfit;  // Return adventure game profit for the user based on UID (now a String)
     }
 
+    // Method to get stocks profits for the user
     public List<Double> getStocksProfit(Long uid) {
-        return stocksProfit;
+        return stocksProfit;  // Return stock profits for the user based on UID (now a String)
     }
 
+    // Method to request a loan
     public void requestLoan(double loanAmount) {
-        this.loanAmount += loanAmount;
-        this.balance += loanAmount;
+        this.loanAmount += loanAmount;  // Increase the loan amount
+        this.balance += loanAmount;  // Add the loan amount to the balance
     }
 
+    // Method to get the current loan amount
     public double getLoanAmount() {
         return loanAmount;
     }
 
+    // Method to calculate daily interest on the loan
     public double dailyInterestCalculation() {
-        double interestRate = 0.05;
+        double interestRate = 0.05;  
         return loanAmount * interestRate;
     }
 }
