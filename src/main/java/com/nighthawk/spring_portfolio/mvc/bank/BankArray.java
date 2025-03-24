5a8582a205ff62cc6dd584bf3d8c3375755bf932
Saaras859 <kodalisaaras@gmail.com>
2025-03-24 11:40:16 -0700

    update

--- src/main/java/com/nighthawk/spring_portfolio/mvc/bank/BankArray.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/bank/BankArray.java
@@ -1,15 +1,11 @@
 package com.nighthawk.spring_portfolio.mvc.bank;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.nighthawk.spring_portfolio.mvc.person.Person;
 import com.nighthawk.spring_portfolio.mvc.person.PersonJpaRepository;
@@ -27,6 +23,7 @@ public class BankArray {
     private UserStocksRepository userStocksRepo;
 
     private Map<String, Double> profitMap = new HashMap<>(); // Stores user profits
+    private Map<String, List<BankRequest>> profitHistoryMap = new HashMap<>(); // Stores profit history logs per user
 
     // DTO for updating profits
     public static class BankRequest {
@@ -40,6 +37,11 @@ public class BankArray {
         public void setAmount(double amount) { this.amount = amount; }
         public String getSource() { return source; }
         public void setSource(String source) { this.source = source; }
+
+        @Override
+        public String toString() {
+            return "Source: " + source + ", Amount: $" + amount;
+        }
     }
 
     @PostMapping("/updateProfit")
@@ -60,8 +62,15 @@ public class BankArray {
         // Append profit history
         String profitHistory = userStocks.getCryptoHistory() + request.getSource() + " profit: $" + request.getAmount() + "\n";
         userStocks.setCryptoHistory(profitHistory);
-        
         userStocksRepo.save(userStocks);
+
+        // Store request in profit history map
+        profitHistoryMap.putIfAbsent(request.getUid(), new ArrayList<>());
+        profitHistoryMap.get(request.getUid()).add(request);
+
+        // Print logs
+        System.out.println("Current profitMap: " + profitMap);
+        System.out.println("Updated profit history for " + request.getUid() + ": " + profitHistoryMap.get(request.getUid()));
 
         return new ResponseEntity<>("Profit updated successfully", HttpStatus.OK);
     }
@@ -73,6 +82,17 @@ public class BankArray {
         if (!profitMap.containsKey(uid)) {
             return new ResponseEntity<>("No profit data found", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(profitMap.get(uid), HttpStatus.OK);
+        System.out.println("Current profitMap: " + profitMap);
+        return new ResponseEntity<>("Total Profit: $" + profitMap.get(uid), HttpStatus.OK);
+    }
+
+    // Get profit history for a user
+    @PostMapping("/getProfitHistory")
+    public ResponseEntity<?> getProfitHistory(@RequestBody Map<String, String> request) {
+        String uid = request.get("uid");
+        if (!profitHistoryMap.containsKey(uid)) {
+            return new ResponseEntity<>("No profit history found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(profitHistoryMap.get(uid), HttpStatus.OK);
     }
 }
