e1a551e563507e36ce57d1188d2db3fc4ab6bdc0
Saaras859 <kodalisaaras@gmail.com>
2025-04-03 12:22:01 -0700

    update changes

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -51,7 +51,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-
 /**
  * Person is a POJO, Plain Old Java Object.
  * --- @Data is Lombox annotation
