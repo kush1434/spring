f9e851f5a2045b78b05ef63122aeac7d7067492a
aaguy-hue <aaditmathur6@gmail.com>
2025-05-26 22:04:48 -0700

    finish lesson mostly

--- src/main/java/com/nighthawk/spring_portfolio/hacks/tablesaw/WineML.java
+++ src/main/java/com/nighthawk/spring_portfolio/hacks/tablesaw/WineML.java
@@ -3,26 +3,24 @@ package com.nighthawk.spring_portfolio.hacks.tablesaw;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import tech.tablesaw.api.*;
-import tech.tablesaw.columns.Column;
-import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.api.Histogram;
 import tech.tablesaw.plotly.api.ScatterPlot;
 import tech.tablesaw.plotly.components.Figure;
 
-import smile.classification.*;
+import weka.core.*;
+import weka.classifiers.evaluation.Evaluation;
 import smile.data.*;
 import smile.data.formula.Formula;
-import smile.data.vector.DoubleVector;
 import smile.data.vector.IntVector;
 import smile.math.MathEx;
 import smile.validation.metric.Accuracy;
 
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 
 
 public class WineML {
@@ -74,12 +72,14 @@ public class WineML {
         int[] indices = IntStream.range(0, n).toArray();
         MathEx.permutate(indices); 
 
-        int splitIndex = (int)(n * 0.75);
+        int splitIndex = (int)(n * 0.8);
         System.out.println(splitIndex + " rows for training, " + (n - splitIndex) + " rows for testing.");
 
         DataFrame trainDf = df.slice(0, splitIndex);
         DataFrame testDf = df.slice(splitIndex, n);
-        RandomForest rf = RandomForest.fit(Formula.lhs("quality"), trainDf);
+
+        // you should normally just import randomforest but I don't want it to conflict with the Weka RandomForest class
+        smile.classification.RandomForest rf = smile.classification.RandomForest.fit(Formula.lhs("quality"), trainDf);
 
         waitUntilEnter();
 
@@ -93,6 +93,69 @@ public class WineML {
 
         double accuracy = Accuracy.of(yTrue, yPred);
         System.out.printf("Model Accuracy: %.2f%%%n", accuracy * 100);
+
+        waitUntilEnter();
+
+
+        // here we convert the Tablesaw table to Weka Instances (kind of like what we did w/ the dataframe above)
+        ArrayList<Attribute> attributes = new ArrayList<>();
+        for (String col : wine.columnNames()) {
+            if (!col.equals("quality")) {
+                attributes.add(new Attribute(col));
+            }
+        }
+
+        // we make an int column for the quality bc it is the classification
+        IntColumn qualityCol = (IntColumn) wine.intColumn("quality");
+        int minQuality = (int) qualityCol.min();
+        int maxQuality = (int) qualityCol.max();
+        ArrayList<String> qualityVals = new ArrayList<>();
+        for (int i = minQuality; i <= maxQuality; i++) {
+            qualityVals.add(String.valueOf(i));
+        }
+        attributes.add(new Attribute("quality", qualityVals));
+
+        Instances wData = new Instances("Wine", attributes, wine.rowCount());
+        wData.setClassIndex(wData.numAttributes() - 1); // ok so this sets the last attribute (quality) as the target variable during classification
+
+        for (int i = 0; i < wine.rowCount(); i++) {
+            double[] vals = new double[wData.numAttributes()];
+            for (int j = 0; j < wine.columnCount() - 1; j++) {
+                vals[j] = ((NumberColumn<?,?>) wine.column(j)).getDouble(i);
+            }
+            vals[wData.numAttributes() - 1] = qualityVals.indexOf(String.valueOf(qualityCol.get(i)));
+            wData.add(new DenseInstance(1.0, vals));
+        }
+
+        // same 80/20 split as before
+        int trainSize = (int) Math.round(wData.numInstances() * 0.8);
+        int testSize = wData.numInstances() - trainSize;
+        
+        Instances train = new Instances(wData, 0, trainSize);
+        Instances test = new Instances(wData, trainSize, testSize);
+        
+        waitUntilEnter();
+
+
+        // time to train the Weka RandomForest model! yayyyyy
+        weka.classifiers.trees.RandomForest wekaRf = new weka.classifiers.trees.RandomForest();
+        try {
+            wekaRf.buildClassifier(train);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        waitUntilEnter();
+
+
+        // test the model
+        try {
+            Evaluation eval = new Evaluation(train);
+            eval.evaluateModel(wekaRf, test);
+            System.out.printf("Weka RandomForest Accuracy: %.2f%%%n", eval.pctCorrect());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
