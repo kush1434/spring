ec81685342b1f03d9d2c615736825b9a61c713f6
hanlunli <hanlunli.007@gmail.com>
2025-05-28 12:29:29 -0700

    merge

--- src/main/java/com/open/spring/mvc/bank/BankApiController.java
+++ src/main/java/com/open/spring/mvc/bank/BankApiController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
-
 import com.open.spring.mvc.person.Person;
 import com.open.spring.mvc.person.PersonJpaRepository;
 
