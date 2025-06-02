2846ad98601d99a2268a2cf0e15c729f03da071f
iKAN2025 <142475176+iKAN2025@users.noreply.github.com>
2025-06-01 17:33:41 -0700

    Revert "Newloginprofileanalytics"

--- src/main/java/com/open/spring/mvc/trains/TrainOrder.java
+++ src/main/java/com/open/spring/mvc/trains/TrainOrder.java
@@ -1,14 +1,14 @@
 package com.open.spring.mvc.trains;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.vladmihalcea.hibernate.type.json.JsonType;
-
 import java.util.*;
 
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import jakarta.persistence.CascadeType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vladmihalcea.hibernate.type.json.JsonType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
@@ -17,33 +17,31 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Entity
 @Data
 @Convert(attributeName = "train", converter = JsonType.class)
-public class Train {
+public abstract class TrainOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="order_id")
     private Long id;
 
     @JsonIgnore
     @ManyToOne
-    @JoinColumn(name="company_id")
-    private TrainCompany company;
+    @JoinColumn(name="train_id")
+    private Train train;
 
-    @OneToMany(mappedBy = "train", cascade = CascadeType.ALL)
-    @JsonIgnore
-    private List<TrainOrder> trainOrders;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.NONE)
+    private Date lastTime;
 
-    @Min(value=-10000)
-    @Max(value=10000)
-    private Float position;
+    private boolean repeat; //may have different effects depending on SubClass interpretation
 
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb")
-    private Map<String,List<Product>> cargo; //Product, amount available
+    private Map<String,String> orderInfo; //Key, Value
+
+    public abstract boolean doSimulation(); //do a single/partial simulation step, returns completion status
 }
--- src/main/resources/static/js/person-extraction.js
+++ src/main/resources/static/js/person-extraction.js
@@ -1,6 +1,5 @@
 import BlobBuilder from "./blob-builder.js";
 
-//Fetch request to extract all people
 async function getAllPeople() {
     let data1;
     await fetch("/mvc/extract/all/person", {
@@ -12,19 +11,12 @@ async function getAllPeople() {
     return data1;
 }
 
-//Ftech request to extract a person with a given id
-async function getSinglePerson(id) {
-    let data1;
-    await fetch("/mvc/extract/person/"+String(id), {
-        method: "GET",
-        cache: "no-cache",
-    }).then((response) => response.json()).then((data) => {
-        data1 = data;
-    })
-    return data1;
-}
+document.getElementById("export-all").addEventListener("click", async () => {
+    let content = await getAllPeople();
+    const blob = new BlobBuilder(BlobBuilder.fileTypeEnum.json, content);
+    blob.downloadBlob("persons");
+})
 
-//fetch request to fetch people in ranges (formatted as: [[i0,i1],[i0,i1]])
 async function getAllPeopleInRanges(ranges) {
     let data1;
     await fetch("/mvc/extract/all/person/fromRanges", {
@@ -62,28 +54,6 @@ document.getElementById("export-ranges").addEventListener("click", async () => {
     let content = await getAllPeopleInRanges(ranges);
     const blob = new BlobBuilder(BlobBuilder.fileTypeEnum.json, content);
     blob.downloadBlob("persons");
-})
-
-document.getElementById("export-selected").addEventListener("click", async () => {
-    let ranges = [];
-    document.getElementsByName("export-select").forEach((input)=>{
-        if(input.checked){
-            let id = Number(input.getAttribute("export-select-id"));
-            ranges.push([id,id]);
-        }
-    })
-    console.log(ranges);
-    let content = await getAllPeopleInRanges(ranges);
-    const blob = new BlobBuilder(BlobBuilder.fileTypeEnum.json, content);
-    blob.downloadBlob("persons");
-})
-
-document.getElementsByName("export").forEach((button)=>{
-    button.addEventListener("click",async ()=>{
-        let content = await getSinglePerson(button.getAttribute("export-id"));
-        const blob = new BlobBuilder(BlobBuilder.fileTypeEnum.json, content);
-        blob.downloadBlob("person");
-    })
 })
 
 
