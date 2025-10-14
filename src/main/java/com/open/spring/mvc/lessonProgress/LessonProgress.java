c005bab041c7943dfb88710d652f9695a08e6c1f
vibha1019 <vibhamandayam08@gmail.com>
2025-10-13 22:25:35 -0700

    fixed entity

--- src/main/java/com/open/spring/mvc/lessonProgress/LessonProgress.java
+++ src/main/java/com/open/spring/mvc/lessonProgress/LessonProgress.java
@@ -2,13 +2,15 @@ package com.open.spring.mvc.lessonProgress;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "lesson_progress")
 @Data
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class LessonProgress {
 
     @Id
@@ -32,11 +34,11 @@ public class LessonProgress {
     @Column(nullable = false)
     private Boolean completed = false;
 
-    // Badges
-    @ElementCollection
+    // Badges - FIXED: Use EAGER fetch to avoid lazy loading issues
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "lesson_badges", joinColumns = @JoinColumn(name = "lesson_progress_id"))
     @Column(name = "badge_name")
-    private Set<String> badges = new HashSet<>();
+    private List<String> badges = new ArrayList<>();
 
     // Current flashcard index (optional)
     @Column
