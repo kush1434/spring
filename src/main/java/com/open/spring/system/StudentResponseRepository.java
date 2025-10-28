3095b327523cbe005c660c849a16e962d54a1c4a
“Ahaanv19” <ahaanvk@gmail.com>
2025-10-28 10:32:09 -0700

    Added backend fetching for frontend lesson

---
+++ src/main/java/com/open/spring/system/StudentResponseRepository.java
@@ -0,0 +1,4 @@
+package com.open.spring.system;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentResponseRepository extends JpaRepository<StudentResponse, Long> {}
