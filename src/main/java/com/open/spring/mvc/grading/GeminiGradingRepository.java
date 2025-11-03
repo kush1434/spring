22a92bf3a10c0615c690d1cb02a31b5902b91e62
aadibhat09 <aadibhat09@gmail.com>
2025-11-02 16:34:09 -0800

    replace unused gemini grader, should work now

--- src/main/java/com/open/spring/mvc/grading/GeminiGradingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.open.spring.mvc.grading;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GeminiGradingRepository extends JpaRepository<GeminiGrading, Long> {
-    // ...existing code...
-}
