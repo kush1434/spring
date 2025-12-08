51c0eeb1985c95c412e75a9643f60429a287a679
adikatre <adityad39c@gmail.com>
2025-12-07 22:55:20 -0800

    feat: Revamp all the groups apis. only GET method.

--- src/main/java/com/open/spring/mvc/groups/Groups.java
+++ src/main/java/com/open/spring/mvc/groups/Groups.java
@@ -4,28 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.open.spring.mvc.person.Person;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @EqualsAndHashCode(callSuper = true)
@@ -33,19 +23,17 @@ import org.hibernate.annotations.NotFoundAction;
 @Getter
 @Setter
 public class Groups extends Submitter {
-    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(
         name = "group_members", 
         joinColumns = @JoinColumn(name = "group_id"), 
         inverseJoinColumns = @JoinColumn(name = "person_id")
     )
-    @NotFound(action = NotFoundAction.IGNORE)
-    @JsonManagedReference
     @JsonIgnore
     private List<Person> groupMembers = new ArrayList<>();
 
-    private String name; // New column for group name
-    private String period; // New column for group period
+    private String name;
+    private String period;
 
     public Groups() {
     }
