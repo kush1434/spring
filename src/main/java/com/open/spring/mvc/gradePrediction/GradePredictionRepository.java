0b7353ee507d9e31a889d93f491f309403b972c6
aadibhat09 <aadibhat09@gmail.com>
2025-12-13 16:01:26 -0800

    remove grade prediction table (it was unneeded)

--- src/main/java/com/open/spring/mvc/gradePrediction/GradePredictionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.open.spring.mvc.gradePrediction;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GradePredictionRepository extends JpaRepository<GradePrediction, Long> {
-}
