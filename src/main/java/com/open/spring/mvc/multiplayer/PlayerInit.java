da99c15d34db234729de83ea8afb7ea2664a7cc1
anonymous-dyce <shauryasinghus@gmail.com>
2026-01-25 20:17:21 -0800

    added try/exception for player database as it was causing errors in running springboot

--- src/main/java/com/open/spring/mvc/multiplayer/PlayerInit.java
+++ src/main/java/com/open/spring/mvc/multiplayer/PlayerInit.java
@@ -13,32 +13,36 @@ public class PlayerInit {
     @Bean
     CommandLineRunner initGamers(PlayerRepository repository) {
         return args -> {
+            try {
+                if (repository.count() == 0) {
 
-            if (repository.count() == 0) {
+                    int count = 50;
+                    String[] uids = new String[count];
+                    for (int i = 0; i < count; i++) {
+                        uids[i] = "player" + (i + 1);
+                    }
 
-                int count = 50;
-                String[] uids = new String[count];
-                for (int i = 0; i < count; i++) {
-                    uids[i] = "player" + (i + 1);
+                    Random random = new Random();
+
+                    for (String uid : uids) {
+                        Player player = new Player();
+                        player.setUid(uid);
+                        player.setStatus("offline");
+                        player.setLevel(random.nextInt(10) + 1);
+                        player.setX(random.nextDouble() * 1000.0);
+                        player.setY(random.nextDouble() * 1000.0);
+                        player.setLastActive(LocalDateTime.now());
+                        player.setConnectedAt(LocalDateTime.now());
+
+                        repository.save(player);
+                    }
+
+                    System.out.println("Gamer database initialized with "
+                            + uids.length + " players");
                 }
-
-                Random random = new Random();
-
-                for (String uid : uids) {
-                    Player player = new Player();
-                    player.setUid(uid);
-                    player.setStatus("offline");
-                    player.setLevel(random.nextInt(10) + 1);
-                    player.setX(random.nextDouble() * 1000.0);
-                    player.setY(random.nextDouble() * 1000.0);
-                    player.setLastActive(LocalDateTime.now());
-                    player.setConnectedAt(LocalDateTime.now());
-
-                    repository.save(player);
-                }
-
-                System.out.println("Gamer database initialized with "
-                        + uids.length + " players");
+            } catch (Exception e) {
+                // Players table may not exist yet - skip initialization
+                System.out.println("Skipping player initialization: " + e.getMessage());
             }
         };
     }
