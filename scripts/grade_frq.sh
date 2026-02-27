2dd717a38b0f9bb4d16f312976803b9d411f65b5
kush1434 <kushkshah3@gmail.com>
2026-02-27 13:39:11 -0800

    updated grading bash script

--- scripts/grade_frq.sh
+++ scripts/grade_frq.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set +H
+source "$(dirname "$0")/../.env"
 
 TOKEN=$(curl -s -X POST http://localhost:8585/authenticate \
   -H "Content-Type: application/json" \
-  -d '{"uid": "alan", "password": "123Qwerty!"}' \
+  -d '{"uid": "'${ADMIN_UID}'", "password": "'${ADMIN_PASSWORD}'"}' \
   -c - | grep jwt_java_spring | awk '{print $NF}')
+
+echo "Authenticated. Currently Grading..."
 
 curl -s -X POST http://localhost:8585/api/grade-frqs \
   --cookie "jwt_java_spring=$TOKEN"
