a1df94430d330ded5594c33e8874219556845bdc
jm1021 <jmort1021@gmail.com>
2026-02-27 14:06:13 -0800

    Revert "Merge pull request #95 from Tvick22/master"
    This reverts commit c0e87e6c367365433ab6c0fa52b779a2ffd5fc2e, reversing
    changes made to 625a98c66668dc12b9103da49ec8c62128e75e1e.

--- src/main/java/com/open/spring/mvc/certificate/UserCertificate.java
+++ src/main/java/com/open/spring/mvc/certificate/UserCertificate.java
@@ -6,8 +6,6 @@ import com.open.spring.mvc.person.Person;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,16 +23,6 @@ public class UserCertificate {
     @ManyToOne(targetEntity = Certificate.class, optional = false)
     private Certificate certificate;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "certificate_type")
-    private CertificateType certificateType;
-
-    @Column(name = "sprint_name")
-    private String sprintName;
-
-    @Column(name = "average_score")
-    private Double averageScore;
-
     @Column(nullable = false)
     private Date dateIssued;
 
@@ -45,18 +33,6 @@ public class UserCertificate {
     public UserCertificate(Person person, Certificate certificate) {
         this.person = person;
         this.certificate = certificate;
-        this.certificateType = CertificateType.COMPLETION; // default
-        this.sprintName = "";
-        this.averageScore = 0.0;
-        this.dateIssued = new Date();
-    }
-
-    public UserCertificate(Person person, Certificate certificate, CertificateType certificateType, String sprintName, Double averageScore) {
-        this.person = person;
-        this.certificate = certificate;
-        this.certificateType = certificateType;
-        this.sprintName = sprintName;
-        this.averageScore = averageScore;
         this.dateIssued = new Date();
     }
 
@@ -90,29 +66,5 @@ public class UserCertificate {
 
     public void setDateIssued(Date dateIssued) {
         this.dateIssued = dateIssued;
-    }
-
-    public CertificateType getCertificateType() {
-        return certificateType;
-    }
-
-    public void setCertificateType(CertificateType certificateType) {
-        this.certificateType = certificateType;
-    }
-
-    public String getSprintName() {
-        return sprintName;
-    }
-
-    public void setSprintName(String sprintName) {
-        this.sprintName = sprintName;
-    }
-
-    public Double getAverageScore() {
-        return averageScore;
-    }
-
-    public void setAverageScore(Double averageScore) {
-        this.averageScore = averageScore;
     }
 }
