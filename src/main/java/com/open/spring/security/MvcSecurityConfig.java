401d213ea3521bd1522251651026cab381dcaf34
adikatre <adityad39c@gmail.com>
2026-03-24 10:30:08 -0700

    works on my machine! ws chat

--- src/main/java/com/open/spring/security/MvcSecurityConfig.java
+++ src/main/java/com/open/spring/security/MvcSecurityConfig.java
@@ -105,6 +105,7 @@ public class MvcSecurityConfig {
                 .requestMatchers("/mvc/grades/**").hasAuthority("ROLE_ADMIN")
                 .requestMatchers("/mvc/assignments/read").hasAnyAuthority("ROLE_ADMIN", "ROLE_TEACHER")
                 .requestMatchers("/mvc/bank/read").hasAuthority("ROLE_ADMIN")
+                .requestMatchers(HttpMethod.OPTIONS, "/ws-chat/**").authenticated()
                 .requestMatchers("/mvc/progress/read").hasAnyAuthority("ROLE_ADMIN", "ROLE_TEACHER")
                 .requestMatchers("/ws-chat/**").authenticated()
                 .requestMatchers("/run/**").permitAll()  // Java runner endpoints - public access
